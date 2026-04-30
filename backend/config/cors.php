ddf0666acb2e1be731b07e05fb5917833dfcdf32
Eddy <eduards.levsa@gmail.com>
2026-04-30 15:04:27 +0300

    cors update

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000','https://easy-rent-gamma.vercel.app',],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
